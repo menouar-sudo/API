ba34f3a2f070ea237ddfd620e398f5f9ff542b7c
menouar-sudo <menouarbarkat@gmail.com>
2026-01-30 01:27:18 +0100

    modified:   main.py 	modified:   pokemon.py

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import requests
 from PIL import Image
 from io import BytesIO
 # number at the end of the URL represents the pokemon ID, each set of 4 represents same pokemon
-url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"
-
-response = requests.get(url, timeout=10)
-response.raise_for_status()  # VERY important
-
-img = Image.open(BytesIO(response.content))
-img.load()   # force validation
-img.show()
+def fetch_pokemon_image(url):
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()  # VERY important
+    img = Image.open(BytesIO(response.content))
+    img.load()   # force validation
+    img.show()
